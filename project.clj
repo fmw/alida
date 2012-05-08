05e5515ac8df33c2fd986cef305d5f23487d00b9
Filip de Waard <fmw@vix.io>
2012-05-08 19:26:39 +0200

    updated project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,12 @@
 (defproject alida "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :description "Crawling, scraping and indexing application."
+  :url "https://github.com/fmw/alida"
+  :license {:name "Apache License, version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-http "0.4.0"]
+                 [enlive "1.0.0-SNAPSHOT"]
+                 [org.jsoup/jsoup "1.6.1"]
+                 [com.ashafa/clutch "0.4.0-SNAPSHOT"]]
+  :dev-dependencies [[radagast "1.1.0"]]
+  :plugins [[lein-swank "1.4.4"]])
